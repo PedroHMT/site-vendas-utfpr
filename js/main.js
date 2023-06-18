75e141eb0767fcedea1f464018a8c1f4aab60aa9
Pedro Trianoski <93216722+PedroTrianoski@users.noreply.github.com>
2023-06-18 16:57:02 -0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    Infinite: true,
+    speed: 300,
+    slideToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
